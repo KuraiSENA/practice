eff1d512029cb81c939ec2646791bebd82d668cc
KuraiSN <david.monroy@soy.sena.edu.co>
2023-10-10 10:31:16 -0500

    first steps

---
+++ projects/first-steps/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <article>
+      <header>
+        <img
+          src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
+          alt="icon"
+        />
+        <div>
+          <strong>Kurai</strong>
+          <br />
+          <span>@kurai2610</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
