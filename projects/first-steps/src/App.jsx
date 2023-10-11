fb875eac7e80513b5cd54cc9ebbf7eb085a998dd
KuraiSN <david.monroy@soy.sena.edu.co>
2023-10-11 10:42:56 -0500

    Changes in the code

--- projects/first-steps/src/App.jsx
+++ projects/first-steps/src/App.jsx
@@ -1,22 +1,6 @@
 import "./App.css";
+import { TwitterCard } from "./TwitterCard";
 
 export function App() {
-  return (
-    <article>
-      <header>
-        <img
-          src="https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png"
-          alt="icon"
-        />
-        <div>
-          <strong>Kurai</strong>
-          <br />
-          <span>@kurai2610</span>
-        </div>
-      </header>
-      <aside>
-        <button>Seguir</button>
-      </aside>
-    </article>
-  );
+  return <TwitterCard name="Kurai" username="Kurai" />;
 }
---
+++ projects/first-steps/src/TwitterCard.jsx
@@ -0,0 +1,21 @@
+export function TwitterCard({ username, name, isFollowing }) {
+  return (
+    <article className="TwitterCard">
+      <header className="TwitterCard-header">
+        <img
+          src={`https://unavatar.io/${username}`}
+          alt="icon"
+          className="TwitterCard-img"
+        />
+        <div className="TwitterCard-info">
+          <strong>{username}</strong>
+          <br />
+          <span className="TwitterCard-info">@{name}</span>
+        </div>
+      </header>
+      <aside className="TwitterCard-aside">
+        <button className="TwitterCard-followButton">Seguir</button>
+      </aside>
+    </article>
+  );
+}
